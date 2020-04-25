c4306f3678f7091540ded25dc06d596d723c1e96
nlarsen7 <nlarsen2013@gmail.com>
2020-04-25 21:31:59 +0000

    Swap

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+//Nicholas Larsen
+//4/25/2020
+//asks for 2 number and outputs the before and after the swapping of the 2 numbers
 package main
 
 import "fmt"
@@ -9,8 +12,11 @@ func integers() {
   fmt.Scanln(&num1)
   fmt.Println("enter another single digit number")
   fmt.Scanln(&num2)
+  //gets the 2 numbers
   fmt.Println("before the swap, The first number is",num1,"and the second is",num2)
+  //before the swap
   fmt.Println("after the swap, The first number is",num2,"and the second is",num1)
+  //after the swap
 }
 
 func main() {
